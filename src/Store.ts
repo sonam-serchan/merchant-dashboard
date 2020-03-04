a6afcbdf1ce79659eda3b71b1c8d52d34a0d3d9b
Ferdi Gökdemir <lucy@Ferdis-MacBook-Pro.local>
2020-03-04 12:07:07 +0300

    general

--- src/Store.ts
+++ src/Store.ts
@@ -1,30 +1,27 @@
-import { action, computed, observable } from 'mobx';
-import { persist } from 'mobx-persist';
-import AuthService from './core/services/auth';
+import { action, computed, observable } from "mobx";
+import { persist } from "mobx-persist";
+import AuthService from "./core/services/auth";
 
 export default class AppStore {
-    @persist
-    @observable token: string | null = null;
-    authService = new AuthService();
+  @persist
+  @observable
+  token: string | null = null;
+  authService = new AuthService();
 
-    constructor() {
+  @computed
+  get isLoggedIn(): boolean {
+    return this.token ? true : false;
+  }
 
-    }
+  @action
+  logout = (): void => {
+    this.authService.logout();
+    this.token = null;
+  };
 
-    @computed
-    get isLoggedIn(): boolean {
-        return this.token ? true : false;
-    }
-
-    @action
-    logout = (): void => {
-        this.authService.logout();
-        this.token = null;
-    };
-
-    @action
-    login = async (): Promise<void> => {
-        const token = await this.authService.login('gkdmr.frd@gmail.com', '123');
-        this.token = token;
-    };
+  @action
+  login = async (): Promise<void> => {
+    const token = await this.authService.login("gkdmr.frd@gmail.com", "123");
+    this.token = token;
+  };
 }
